cd3edd58a2b5b6747d2884d45a1306940151b86b
LucasFerreiraRodrigue <lucas_f_rodrigues@hotmail.com>
2016-07-18 18:19:49 -0300

    Adição das entidades e começo das classes de controle

---
+++ src/main/java/bean/Aluno.java
@@ -0,0 +1,35 @@
+package bean;
+
+public class Aluno {
+
+	private Integer id;
+	private String nome;
+	private String usuario;
+	private String senha;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
---
+++ src/main/java/bean/Materia.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class Materia {
+
+	private String nome;
+	private Integer cargaHoraria;
+	private Integer quantidadeAvaliacoes;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getCargaHoraria() {
+		return cargaHoraria;
+	}
+	public void setCargaHoraria(Integer cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	public Integer getQuantidadeAvaliacoes() {
+		return quantidadeAvaliacoes;
+	}
+	public void setQuantidadeAvaliacoes(Integer quantidadeAvaliacoes) {
+		this.quantidadeAvaliacoes = quantidadeAvaliacoes;
+	}
+}
