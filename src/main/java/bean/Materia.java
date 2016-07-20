28a23d7b1cf8d7cd71b219a1e79411849c2dbaa4
LucasFerreiraRodrigue <lucas_f_rodrigues@hotmail.com>
2016-07-20 00:19:24 -0300

    Mapeamento da classe Materia

--- src/main/java/bean/Materia.java
+++ src/main/java/bean/Materia.java
@@ -1,15 +1,27 @@
 package bean;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "materia", schema = "public")
 public class Materia {
 
 	@Id
+	@GeneratedValue
+	@Column(name = "id")
 	private Integer id;
+	
+	@Column(name = "nome")
 	private String nome;
+	
+	@Column(name = "caraga_horaria")
 	private Integer cargaHoraria;
+	
+	@Column(name = "quantidade_avaliacoes")
 	private Integer quantidadeAvaliacoes;
 
 	public Integer getId() {
